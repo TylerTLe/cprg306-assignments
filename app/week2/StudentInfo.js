775d24aa42bc4d10dbdd9fe252e7b4a4e8ed8f57
Tyler <tyler.le@edu.sait.ca>
2023-09-18 16:07:47 -0600

    completes main page and week2 page

---
+++ app/week2/StudentInfo.js
@@ -0,0 +1,15 @@
+const StudentInfo = () => {
+  return (
+    <main>
+      <div>
+        <p>Name: Tyler Le</p>
+        <p>Course section: CPRG 306 D</p>
+        <p>
+          <a href="https://github.com/TylerTLe">https://github.com</a>
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default StudentInfo;
---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from "./StudentInfo"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl">My Shopping List</h1>
+        <StudentInfo />
+      </div>
+    </main>
+  )
+}
