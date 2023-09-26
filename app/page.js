34c0ac3fbcd37bc4ae24eac38a9f3c160c2abfe6
Tyler <tyler.le@edu.sait.ca>
2023-09-26 08:36:52 -0600

    fixes error and changes background colour

--- app/page.js
+++ app/page.js
@@ -1,27 +1,29 @@
+import Link from "next/link";
+import link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-t from-gray-700 via-gray-900 to-black">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className="text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
         <div>
           <p>Name: Tyler Le</p>
           <p>Course section: CPRG 306 D</p>
-          <p>
+          <div>
             <a href="https://github.com/TylerTLe">https://github.com</a>
             <ul>
               <li>
-                <a href="week2">Week 2</a>
+                <Link href="week2">Week 2</Link>
               </li>
               <li>
-                <a href="week3">Week 3</a>
+                <Link href="week3">Week 3</Link>
               </li>
               <li>
                 {/* Inset week4 when needed */}
-                <a href="#">Week 4</a>
+                <Link href="#">Week 4</Link>
               </li>
             </ul>
-          </p>
+          </div>
         </div>
       </div>
     </main>
